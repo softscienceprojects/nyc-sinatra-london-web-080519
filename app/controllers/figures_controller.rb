fa615f4307289cfd1c91fd9c3044095929a4aaea
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-01 10:14:56 +0100

    10 failures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class FiguresController < ApplicationController
   # add controller methods
 
@@ -15,19 +17,33 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
-  post '/figures' do
-    @figure = Figure.create(params[:figure])
-      @figure.titles << Title.create(name: params["title"]["name"])
-  
-
-    redirect '/figures/#{@figure.id}'
-  end
-
   #Show
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+
+  #create
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure]["name"])
+  
+      if params["title"]["name"].empty?
+        @figure.titles << Title.create(name: params["title"]["name"])
+      end
+      if params["landmark"]["name"].empty?
+        @figure.landmarks << Landmark.create(name: params["landmark"]["name"], year_completed: ["landmark"]["year_completed"])
+      end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  #edit
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+
 
 
 end
